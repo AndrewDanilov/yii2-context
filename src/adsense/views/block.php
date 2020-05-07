640c613be8a7dce0eea3142c980ff2aa2cdefbf1
andrey <6097033725>
2020-05-08 00:22:53 +0300

    Misc

--- src/adsense/views/block.php
+++ src/adsense/views/block.php
@@ -10,9 +10,9 @@
      style="display:block"
      data-ad-client="<?= $dataAdClient ?>"
      data-ad-slot="<?= $dataAdSlot ?>"
-     <?php if ($dataAdLayout !== null) { ?>data-ad-layout="<?= $dataAdLayout ?>"<?php } ?>
      data-ad-format="<?= $dataAdFormat ?>"
-     data-full-width-responsive="<?= $dataFullWidthResponsive ? 'true' : 'false' ?>"></ins>
+     <?php if ($dataAdLayout !== null) { ?>data-ad-layout="<?= $dataAdLayout ?>"<?php } ?>
+     <?php if ($dataFullWidthResponsive) { ?>data-full-width-responsive="true"<?php } ?>></ins>
 <script>
 	(adsbygoogle = window.adsbygoogle || []).push({});
 </script>
